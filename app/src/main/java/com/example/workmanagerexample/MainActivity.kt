7bb5d7bf827d486786262dd5d9f37eecddeb8a0b
ahmed <android1@codecaique.com>
2020-10-05 23:35:58 +0200

    add foreground code

--- app/src/main/java/com/example/workmanagerexample/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerexample/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.workmanagerexample
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.workmanagerexample.Constants.Companion.STATE_INPUT
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +18,20 @@ class MainActivity : AppCompatActivity() {
             .putString(STATE_INPUT, "Hey I am sending the work data")
             .build()
 
-        val request = OneTimeWorkRequest.Builder(MyWorker::class.java).setInputData(data).build()
+        val constraints = Constraints.Builder().apply {
+            setRequiredNetworkType(NetworkType.CONNECTED)
+        }.build()
+
+//        setRequiresCharging(true)
+//        setRequiresStorageNotLow(true)
+
+        val request = OneTimeWorkRequest.Builder(MyWorker::class.java).setInitialDelay(2, TimeUnit.SECONDS).setInputData(data).setConstraints(constraints).build()
+
 
         btnClickedId.setOnClickListener {
 //            Handler().postDelayed(Runnable {
+                WorkManager.getInstance(this).enqueue(request)
 //            },2000)
-            WorkManager.getInstance(this).enqueue(request)
 
         }
 
--- app/src/main/java/com/example/workmanagerexample/MyWorker.kt
+++ app/src/main/java/com/example/workmanagerexample/MyWorker.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.work.*
 import com.example.workmanagerexample.Constants.Companion.STATE_INPUT
@@ -24,9 +25,47 @@ class MyWorker(private val context: Context, private val workerParameters: Worke
 //
 
 
+//        setForegroundAsync(createForegroundInfo(""))
+
         return Result.success()
     }
 
+
+    // Creates an instance of ForegroundInfo which can be used to update the
+    // ongoing notification.
+
+//    private fun createForegroundInfo(progress: String): ForegroundInfo {
+//        val id = applicationContext.getString(R.string.notification_channel_id)
+//        val title = applicationContext.getString(R.string.notification_title)
+//        val cancel = applicationContext.getString(R.string.cancel_download)
+//        // This PendingIntent can be used to cancel the worker
+//        val intent = WorkManager.getInstance(applicationContext)
+//            .createCancelPendingIntent(getId())
+//
+//        // Create a Notification channel if necessary
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            createChannel()
+//        }
+//
+//        val notification = NotificationCompat.Builder(applicationContext, id)
+//            .setContentTitle(title)
+//            .setTicker(title)
+//            .setContentText(progress)
+//            .setSmallIcon(R.drawable.ic_work_notification)
+//            .setOngoing(true)
+//            // Add the cancel action to the notification which can
+//            // be used to cancel the worker
+//            .addAction(android.R.drawable.ic_delete, cancel, intent)
+//            .build()
+//
+//        return ForegroundInfo(notification)
+//    }
+//
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    private fun createChannel() {
+//        // Create a Notification channel
+//    }
+//
 
     private fun displayNotification(task: String, desc: String) {
         val manager =
